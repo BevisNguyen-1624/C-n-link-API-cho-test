eb6d78dc203d4e361de232ba0dffdbd79c1df4ef
BevisNguyen-1624 <binhnh@yody.vn>
2026-05-19 19:55:04 +0700

    url

--- src/imports/pasted_text/api-helpers.tsx
+++ src/imports/pasted_text/api-helpers.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 
-const API_URL = "https://script.google.com/macros/s/AKfycbwBWTKdfYEDwpvhYoZnRfT7IGEKLrfOn7sKtvRRbXYwJw1gIp_QVJCeenITcwQDWbE/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbz0lzraHmyE1J8lOrqK-09ETCr1tF5dp9rWHK4-Oexo5VlITd13DKQWxWM2KTK3TAo/exec";
 const USE_MOCK = false;
 const ADMIN_PIN = "1234";
 const LS_KEY = "hoYoTa_reviewers";
