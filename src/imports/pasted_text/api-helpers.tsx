21a6d513a1baecdab4bbf1420f60f602dcae87e8
BevisNguyen1624 <nguyenhuybinh021@gmail.com>
2026-05-23 00:09:45 +0700

    change

--- src/imports/pasted_text/api-helpers.tsx
+++ src/imports/pasted_text/api-helpers.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import bgImage from "../KV_HÒ_YO_TA-01.jpeg";
 import boatImage from "../boat.png";
 
-const API_URL = "https://script.google.com/macros/s/AKfycbyB_o9sPex3tk7DfIIO3RmB7TZdmRe3TO7qShPN5wPz2SAQieYlsjZSkguJKzvKeGU/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzPgOBmM_3xJLN8716to5VE4Y1U7RQTF3obLOY4CY0qMAWd8jAPwLlIW3OgQnFQXA4/exec";
 const USE_MOCK = false;
 const ADMIN_PIN = "1234";
 const LS_KEY = "hoYoTa_reviewers";
